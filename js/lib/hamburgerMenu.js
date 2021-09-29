08f1b8989ec360dfa983a40317f8be0555894bcb
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-09-29 19:29:07 +0200

    styling index page done

--- js/lib/hamburgerMenu.js
+++ js/lib/hamburgerMenu.js
@@ -1,5 +1,5 @@
-const hamburgerButton = document.querySelector(".hamburger-button");
-const navUl = document.querySelector(".nav-ul");
+const hamburgerButton = document.querySelector(".button_hamburger");
+const navUl = document.querySelector(".nav");
 
 hamburgerButton.addEventListener("click", () => {
   navUl.classList.toggle("show");
